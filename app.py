f0d68faf081eb7d97d856df41f70484e1e42e180
divesh-netizen <61093504+divesh-netizen@users.noreply.github.com>
2021-10-16 10:42:11 +0530

    Ner & textPreprocessing

---
+++ app.py
@@ -0,0 +1,21 @@
+# importing the required module
+import spacy
+import wikipediaapi
+import streamlit as st
+import spacy_streamlit
+
+
+# Title of the app
+st.title("Text Preprocessing and NER")
+
+# scrapping data from the wikkipedia for a given name or title
+wiki = wikipediaapi.Wikipedia(language='en',
+                            extract_format=wikipediaapi.ExtractFormat.WIKI)
+user_input = st.text_input("Give Some Name and Any Title", "India")
+user_text = wiki.page(user_input).text
+
+# getting the entity of the all words 
+# from the scrapped data and visualising on streamlit
+nlp = spacy.load("en_core_web_sm")
+res = nlp(user_text)
+spacy_streamlit.visualize_ner(res, labels=nlp.get_pipe('ner').labels)
